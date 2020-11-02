1b9b22f41f7a379d4c946bbbe10d8d1d12cf88d6
81946 <71406891+81946@users.noreply.github.com>
2020-11-02 13:06:50 -0800

    update

--- task1.py
+++ task1.py
@@ -6,3 +6,73 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+
+
+e1=''
+e2=''
+e3=''
+out = StringVar()
+
+
+
+def clickFunction():
+    x= e1.get()
+    x= str(x)
+    y= e2.get()
+    y= str(y)
+    z= e3.get()
+    z= str(z)
+
+    e4.insert(0, "I"+ ' ' + x + ' '+ "to the store. I then sat on a" + ' ' + y + "." + ' ' + "I then saw Billy, they were" + ' ' + z + ".")
+
+
+
+
+
+
+
+
+#----------------------------------
+l1 = Label(win, text="I (Verb)")
+e1 = Entry(win, width=20)
+l2 = Label(win, text="to the store. I then sat on a(Noun)")
+e2 = Entry(win, width=20)
+l3 = Label(win, text="I then saw Billy, they were (Adjective)")
+e3 = Entry(win, width=20)
+
+b1 = Button(win, text="Click to create Madlib", command=clickFunction)
+
+l4 = Label(win, text="The finished Madlib is:")
+e4= Entry(win, width=80, textvariable=out)
+
+
+
+#----------------------------------
+
+
+l1.pack()
+e1.pack()
+l2.pack()
+e2.pack()
+l3.pack()
+e3.pack()
+print("\n")
+b1.pack()
+print("\n")
+l4.pack()
+e4.pack()
+
+
+
+
+
+
+
+
+
+#-------------------------
+win.mainloop()
--- task2.py
+++ task2.py
@@ -12,3 +12,54 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+#---------------------------
+out = StringVar()
+
+
+def clickFunction():
+    b= str(e1)
+    c= str(e2)
+    b= float(b)
+    c= float(c)
+    discriminant= (pow(b,2))-(4*1*c)
+    x= str(discriminant)
+    e3.insert(0, x)
+
+
+
+
+
+
+l1 = Label(win, text="Please enter your 'b' (Top) and your 'c' (Bottom).")
+e1 = Entry(win, width=20)
+e2 = Entry(win, width=20)
+b1 = Button(win, text="Click to find x", command=clickFunction)
+
+l3 = Label(win, text="x=")
+e3= Entry(win, width=80, textvariable=out)
+
+
+#---------------
+
+
+l1.pack()
+e1.pack()
+e2.pack()
+b1.pack()
+l3.pack()
+e3.pack()
+
+
+
+
+
+
+
+
+
+#-----------------------
+win.mainloop()
